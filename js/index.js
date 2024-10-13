a99fbd5e2e9381a78903d534374a725dde3f1586
Robinn023 <114648209+Robinn023@users.noreply.github.com>
2024-10-13 20:35:03 +0200

    Player/pacman class  aangemaakt en start locatie gegeven

--- js/index.js
+++ js/index.js
@@ -5,8 +5,9 @@ const c = canvas.getContext('2d')
     canvas.height = innerHeight 
 
 class Boundary {
+    static width = 40
+    static height = 40
     constructor({position}) {
-        this.position = position
         this.position = position
         this.width = 40
         this.height = 40
@@ -14,30 +15,57 @@ class Boundary {
 
     draw(){
         c.fillStyle = 'blue'
-        c.fillRect(thus.position.x, this.
+        c.fillRect(this.position.x, this.
             position.y, this.width, 
             this.height)
         }
     } 
 
+    class Player  {
+    constructor({ position,velocity }) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = 15
+    }
+    draw(){
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, 
+            Math.PI * 2)
+            c.fillStyle = 'yellow'
+            c.fill()
+            c.closePath()
+    }
+    }
+
     const map = [
         ['-', '-', '-', '-', '-', '-',],
-        ['-', '-', '-', '-', '-', '-',],
-        ['-', '-', '-', '-', '-', '-',],
+        ['-', ' ', ' ', ' ', ' ', '-',],
+        ['-', ' ', '-', '-', ' ', '-',],
+        ['-', ' ', ' ', ' ', ' ', '-',],
         ['-', '-', '-', '-', '-', '-',]
 
       ]
     const boundaries = []
+    const player = new Player({
+        position: {
+            x: Boundary.width + Boundary.width / 2,
+            y: Boundary.height + Boundary.height / 2
+        },
+        velocity: {
+            x: 0,
+            y: 0
+        }
+    })
 
     map.forEach((row, i) => {
-        row.forEach((symbol) => {
+        row.forEach((symbol, j) => {
             switch (symbol) {
                 case '-':
                     boundaries.push(
                         new Boundary({
                             position: {
-                                x: 40,
-                                y: 40 * 1
+                                x: Boundary.width * j,
+                                y: Boundary.height * i
                             }
                         })
                     )
@@ -48,6 +76,12 @@ class Boundary {
 
 boundaries.forEach((boundary) => {
     boundary.draw()
+})
+
+player.draw()
+
+addEventListener('keydown', () =>{
+    
 })
 
     new Boundary({
